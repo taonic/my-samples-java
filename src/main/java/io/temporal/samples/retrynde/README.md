88f4a3d2fd51efdb44b0baa10b33ce0ed57e546b
Tao Guo <tao@temporal.io>
2024-06-13 21:44:55 +1000

    Add README to RetryNDE sample

---
+++ src/main/java/io/temporal/samples/retrynde/README.md
@@ -0,0 +1,25 @@
+# Retryable Non-deterministic Workflow
+
+For some idempotent Workflows, instead of dealing with determinism, one may want the entire workflow to retry from
+scratch when a code change introduces an non-deterministic error during Workflow replay.
+
+This sample fabricates a NDE while leveraging [setFailWorkflowExceptionTypes](https://javadoc.io/doc/io.temporal/temporal-sdk/latest/io/temporal/worker/WorkflowImplementationOptions.Builder.html#setFailWorkflowExceptionTypes(java.lang.Class...))
+and `NonDeterministicException.class` to trigger a Workflow execution failure. It also set the default retry policy to drive the desired retry behaviour.
+
+To run the sample locally:
+```
+./gradlew -q execute -PmainClass=io.temporal.samples.retrynde.RetryNDE
+```
+
+Or against Temporal Cloud:
+```
+./gradlew -q execute -PmainClass=io.temporal.samples.retrynde.RetryNDE --args=" \
+--client-cert-path=<path-to-cert> --client-key-path=<path-to-key> \
+--temporal-endpoint=<namespace>.<account>.tmprl.cloud:7233 --temporal-namespace=<namespace>.<account>"
+```
+
+1. Wait for the message - "You have 10 seconds to restart the worker to trigger an NDE" then rerun the sample.
+
+1. Once the Worker is restarted, you should see "Forcing an NDE to fail the workflow".
+
+1. On the UI, you should see a failed Workflow with ID: `RetryNDEWorkflow` and a newly retried one in the running state.
--- src/main/java/io/temporal/samples/retrynde/RetryNDE.java
+++ src/main/java/io/temporal/samples/retrynde/RetryNDE.java
@@ -19,23 +19,46 @@
 
 package io.temporal.samples.retrynde;
 
+import com.sun.net.httpserver.HttpServer;
+import com.uber.m3.tally.RootScopeBuilder;
+import com.uber.m3.tally.Scope;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowClientOptions;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.common.RetryOptions;
+import io.temporal.common.reporter.MicrometerClientStatsReporter;
+import io.temporal.samples.workertuning.MetricsUtils;
+import io.temporal.serviceclient.SimpleSslContextBuilder;
 import io.temporal.serviceclient.WorkflowServiceStubs;
-import io.temporal.worker.NonDeterministicException;
-import io.temporal.worker.Worker;
-import io.temporal.worker.WorkerFactory;
-import io.temporal.worker.WorkflowImplementationOptions;
+import io.temporal.serviceclient.WorkflowServiceStubsOptions;
+import io.temporal.worker.*;
 import io.temporal.workflow.Workflow;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 import io.temporal.workflow.unsafe.WorkflowUnsafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+import javax.net.ssl.SSLException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import static picocli.CommandLine.*;
+import java.time.Duration;
 
 /**
  * Sample Temporal Workflow Definition that demonstrates retrying Workflow on non-deterministic errors.
  */
-public class RetryNDE {
+@Command(name = "retry_nde_example", description = "")
+public class RetryNDE implements Runnable {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(RetryNDE.class);
+
+    private static HttpServer scrapeEndpoint;
 
     // Define the task queue name
     static final String TASK_QUEUE = "RetryNDETaskQueue";
@@ -43,21 +66,34 @@ public class RetryNDE {
     // Define the workflow unique id
     static final String WORKFLOW_ID = "RetryNDEWorkflow";
 
+    @CommandLine.Option(names = "--temporal-namespace", description = "The Temporal namespace to connect to")
+    static String temporal_namespace;
+
+    @CommandLine.Option(names = "--temporal-endpoint", description = "The Temporal endpoint to connect to")
+    static String temporal_endpoint;
+
+    @CommandLine.Option(names = "--client-key-path", description = "The mTLS client key for authenticating to the namespace")
+    static String client_key_path;
+
+    @CommandLine.Option(names = "--client-cert-path", description = "The mTLS client certificate for authenticating to the namespace")
+    static String client_cert_path;
+
+    @CommandLine.Option(names = "--metric-port", description = "The port used to expose OpenMetrics", defaultValue = "8077")
+    static int metric_port;
+
     @WorkflowInterface
     public interface RetryNDEWorkflow {
         @WorkflowMethod
         String getGreeting();
     }
 
-    public static class GreetingWorkflowImpl implements RetryNDEWorkflow {
+    public static class RetryNDEWorkflowImpl implements RetryNDEWorkflow {
         @Override
         public String getGreeting() {
-            System.out.println("Run ID: " + Workflow.getInfo().getRunId());
-
             if (WorkflowUnsafe.isReplaying()) {
                 // Create a side effect only during replay to induce an non-deterministic error.
-                System.out.println("Forcing an NDE");
-                Workflow.sideEffect(String.class, () -> "forcing an NDE");
+                System.out.println("Forcing an NDE to fail the workflow");
+                Workflow.sideEffect(String.class, () -> "I'm a side effect to cause an NDE");
             } else {
                 if (Workflow.getInfo().getAttempt() == 1) {
                     System.out.println("You have 10 seconds to restart the worker to trigger an NDE");
@@ -72,19 +108,51 @@ public class RetryNDE {
         }
     }
 
-    public static void main(String[] args) {
-        WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
-        WorkflowClient client = WorkflowClient.newInstance(service);
+    private static WorkflowClient runWorker() throws FileNotFoundException, SSLException {
+        // Prepare Prometheus endpoint
+        PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        Scope scope =
+                new RootScopeBuilder()
+                        .reporter(new MicrometerClientStatsReporter(registry))
+                        .reportEvery(com.uber.m3.util.Duration.ofSeconds(1));
+        scrapeEndpoint = MetricsUtils.startPrometheusScrapeEndpoint(registry, metric_port);
+        log.info("Starting Prometheus scrape endpoint at: http://localhost:" + scrapeEndpoint.getAddress().getPort() + "/metrics");
+
+        // Build workflow client
+        WorkflowServiceStubsOptions.Builder wfServiceOptionsBuilder = WorkflowServiceStubsOptions.newBuilder()
+                .setMetricsScope(scope);
+        WorkflowClientOptions.Builder wfClientBuilder = WorkflowClientOptions.newBuilder();
+        if (temporal_namespace != null) {
+            InputStream clientCert = new FileInputStream(client_cert_path);
+            InputStream clientKey = new FileInputStream(client_key_path);
+            wfServiceOptionsBuilder
+                    .setSslContext(SimpleSslContextBuilder.forPKCS8(clientCert, clientKey).build())
+                    .setTarget(temporal_endpoint);
+            wfClientBuilder.setNamespace(temporal_namespace);
+        }
+        WorkflowServiceStubs service = WorkflowServiceStubs.newServiceStubs(wfServiceOptionsBuilder.build());
+        WorkflowClient client = WorkflowClient.newInstance(service, wfClientBuilder.build());
+
+        // Build worker
         WorkerFactory factory = WorkerFactory.newInstance(client);
         Worker worker = factory.newWorker(TASK_QUEUE);
-
-        // Tell workflow to fail on NonDeterministicExceptions
         WorkflowImplementationOptions options =
                 WorkflowImplementationOptions.newBuilder()
                         .setFailWorkflowExceptionTypes(NonDeterministicException.class)
                         .build();
-        worker.registerWorkflowImplementationTypes(options, GreetingWorkflowImpl.class);
+        worker.registerWorkflowImplementationTypes(options, RetryNDE.RetryNDEWorkflowImpl.class);
         factory.start();
+        return client;
+    }
+
+    @Override
+    public void run() {
+        WorkflowClient client;
+        try {
+            client = runWorker();
+        } catch (FileNotFoundException | SSLException e) {
+            throw new RuntimeException("Failed to start worker", e);
+        }
 
         // Set retry policy on workflow. This is usually not recommended, but we are demonstrating a special case
         // that forces workflow re-run on a non-deterministic error.
@@ -93,12 +161,24 @@ public class RetryNDE {
                         RetryNDEWorkflow.class,
                         WorkflowOptions.newBuilder()
                                 .setRetryOptions(RetryOptions.newBuilder().build())
+                                .setWorkflowTaskTimeout(Duration.ofSeconds(1))
                                 .setWorkflowId(WORKFLOW_ID)
                                 .setTaskQueue(TASK_QUEUE)
                                 .build());
 
         String output = workflow.getGreeting();
+
+        try {
+            // leave some time for inspecting metrics
+            Thread.sleep(Duration.ofSeconds(60).toMillis());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(output);
         System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        new CommandLine(new RetryNDE()).execute(args);
     }
 }
